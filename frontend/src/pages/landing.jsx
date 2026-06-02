a15e4ed89ecc54b038cf7afa7ee90a9b89fd20fe
faiz <faizmalik93320@outlook.com>
2026-06-02 20:42:52 +0530

    getStart Route fix

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import "../App.css"
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 export default function LandingPage() {
-  
+
   let router = useNavigate();
 
   return (
@@ -13,29 +14,30 @@ export default function LandingPage() {
           <h2>Apna Zoom</h2>
         </div>
         <div className="navList">
-          <p onClick={() =>{
-            router("/dhkjae")}
+          <p onClick={() => {
+            router("/dhkjae")
+          }
           } >Join as Guest</p>
-          <p onClick={() => {router("/auth")}} >Register</p>
+          <p onClick={() => { router("/auth") }} >Register</p>
           <div role='button'>
-            <p onClick={() => {router("/auth")}}>Login</p>
+            <p onClick={() => { router("/auth") }}>Login</p>
           </div>
         </div>
       </nav>
 
       <div className="landingMainContainer">
         <div>
-          <h1>because <span style={{color:'orange'}}>Distance</span> shouldn't seperate <span style={{color:'#ff7f7f'}}>Hearts</span>.</h1>
+          <h1>because <span style={{ color: 'orange' }}>Distance</span> shouldn't seperate <span style={{ color: '#ff7f7f' }}>Hearts</span>.</h1>
           <p>Bridge the distance with Apna Zoom Call </p>
           <div className='getStart'>
-            <a href="/auth">Get Started</a>
+            <Link to="/auth">Get Started</Link>
           </div>
         </div>
         <div className='png'>
           <img src="/phone2-removebg-preview.png" alt="image" />
         </div>
       </div>
-      
+
     </div>
   )
 }
