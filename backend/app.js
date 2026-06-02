6da5df2d1ebe85051557ad975fdd73324528789f
faiz <faizmalik93320@outlook.com>
2026-06-02 20:33:59 +0530

    add main route

--- backend/app.js
+++ backend/app.js
@@ -11,17 +11,20 @@ const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
 
-app.set("port", (process.env.Port || 8000));
+app.set("port", (process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true})); 
 
 app.use("/api/v1",userRoutes);
-
+app.get("/", (req, res) => {
+  res.send("SyncMeet Backend is running 🚀");
+});
 const start = async ()=>{
   app.set("mongo_user")
     const connectionDb = await mongoose.connect("mongodb+srv://faizmalik_123:xlEY8ifTiqaKjDfG@cluster0.1xwkk3v.mongodb.net/")
     console.log(`MONGO is Connected Host ${connectionDb.connection.host}`);
+
   server.listen(app.get("port"), () => {
     console.log("listening is start")
   });
